92f299deb298f1cbf0bb1362e4c9362194f2d0ff
blip456 <yoranbroodcooren@gmail.com>
2014-12-10 18:07:53 +0100

    RF enzo

---
+++ RF/rf_test1/rf_test1.ino
@@ -0,0 +1,20 @@
+
+// NRF      ARDUINO    COLOR
+// 1 GND    GND        black
+// 2 VCC    3.3V       gray
+// 3 CE     4 out      white
+// 4 CSN    5 out      purple
+// 5 SCK    13 out     blue
+// 6 MOSI   11 out     green
+// 7 MISO   12 in      yellow
+// 8 IRQ    2 in       orange
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
